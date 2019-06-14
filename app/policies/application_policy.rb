d22cac3ce687ac791505b6284efb527ad7d45257
Kaka Ruto <borecollins@gmail.com>
2019-06-14 17:48:46 +0300

    Show auth defaults at the application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,5 +4,33 @@ class ApplicationPolicy < ActionPolicy::Base
   # Inheriting from ActionPolicy::Base adds all the authorization modules
   # described here https://actionpolicy.evilmartians.io/#/custom_policy
 
-  # It also has `authorize :user` by default
+  # The following functionality is available out of the box
+
+  # include ActionPolicy::Policy::Core
+  # include ActionPolicy::Policy::Authorization
+  # include ActionPolicy::Policy::PreCheck
+  # include ActionPolicy::Policy::Reasons
+  # include ActionPolicy::Policy::Aliases
+  # include ActionPolicy::Policy::Cache
+  # include ActionPolicy::Policy::CachedApply
+  # include ActionPolicy::Policy::Defaults
+
+  # ActionPolicy::Policy::Defaults module adds the following
+
+  # authorize :user
+
+  # default_rule :manage?
+  # alias_rule :new?, to: :create?
+
+  # def index?
+  #   false
+  # end
+
+  # def create?
+  #   false
+  # end
+
+  # def manage?
+  #   false
+  # end
 end
